7f7176d9936059d3922c2232d6bd57ea956138eb
Trícia Alves <tricia.alves@acad.ufsm.br>
2025-11-02 15:25:56 -0300

    Delete app directory

--- app/models/cliente.model.ts
+++
@@ -1,9 +0,0 @@
-import { Pessoa } from './pessoa.model';
-export class Cliente extends Pessoa {
-    endereco: string;
-    constructor(nome: string, fone: string, email: string, endereco: string,
-        fotoUrl?: string) {
-        super(nome, fone, email, fotoUrl);
-        this.endereco = endereco;
-    }
-}
